d61ad433c4f83cfc558ea6d94fbddd9c049d455c
Matin <Matin@DESKTOP-QUGOMAD>
2025-01-09 00:33:56 +0330

    start new

--- obj/Debug/net8.0/shop.AssemblyInfo.cs
+++ obj/Debug/net8.0/shop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("shop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+929af9d9c17116088886f6b3f3f82285727e5fa3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("shop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("shop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
