2d4663a1575d7cb9aa584c81b04bed018c3505ae
Matin <Matin@DESKTOP-QUGOMAD>
2025-01-23 19:29:43 +0330

    start_product

---
+++ Areas/Admin/Controller/ProductController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+[Area ("Admin")]
+public class ProductController: Controller
+
+{
+public IActionResult index()
+{
+    // TODO: Your code here
+    return View();
+}
+
+
+}
